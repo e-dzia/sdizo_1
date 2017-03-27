8ca699460153ee7e789ef907a438c3b1ccb80b2a
e-dzia <226000@student.pwr.edu.pl>
2017-03-28 00:48:21 +0200

    Wiem, dlaczego nie działa Otóż jest błąd z usuwaniem korzenia

--- RedBlackTree.cpp
+++ RedBlackTree.cpp
@@ -12,26 +12,24 @@ RedBlackTree::RedBlackTree() {
     g->left = g;
     g->right = g;
     g->color = 'B';
-    g->value = -1;
     this->guard = g;
     this->root = this->guard;
     //cout << "Nowe RBT " << this->root << endl;
 }
 
 RedBlackTree::~RedBlackTree() {
-    //cout << "Usuwam RBT " << this->root << endl;
+    cout << "###############################################################################Usuwam RBT " << this->root << endl;
+    delete this->guard;
     deleteTree(this->root);
     //delete this->root;
-    //delete this->guard;
+
 }
 
 void RedBlackTree::deleteTree(RBTNode *pNode) {
     if (pNode != NULL && pNode != this->guard){
-        //cout << "Usuwam " << pNode->value << " " << pNode->right->value << " " << pNode->left->value << endl;
-        if (pNode->right != pNode)
-            deleteTree(pNode->right);
-        if (pNode->left != pNode)
-            deleteTree(pNode->left);
+        deleteTree(pNode->right);
+        deleteTree(pNode->left);
+        //cout << "Usunieto " << pNode->value << endl;
         delete pNode;
     }
     /*if (pNode->right != this->guard && pNode->right != NULL)
@@ -62,7 +60,7 @@ void RedBlackTree::loadFile(string name) {
 }
 
 void RedBlackTree::addElement(int value, int notUsed) {
-    RBTNode *tmp = this->findEl(value);
+    RBTNode * tmp = this->findEl(value);
     if (tmp != this->guard) return;
     RBTNode* rbt = new RBTNode; //nowy wezel
     rbt->value = value;
@@ -94,75 +92,79 @@ void RedBlackTree::addElement(int value, int notUsed) {
         }
     }
     this->fixRBT_A(rbt);
+    cout << "Korzen po dodaniu: " << this->root->value << endl;
 
 }
 
 void RedBlackTree::deleteElement(int value) {
     RBTNode * p = this->findEl(value);
     if (p != this->guard){
-        cout << "Usuwam " << p->value;
         RBTNode * toChange;
         if (p->left == this->guard && p->right == this->guard){
             toChange = this->guard;
-            cout << " Lisc";
         }
         else {
             if (p->left == this->guard) {
-                cout << " jedenP";
                 toChange = p-> right;
             }
             else if (p->right == this->guard){
-                cout << " jedenL";
                 toChange = p->left;
             }
             else if (p->right != this->guard && p->left != this->guard){
-                cout << " dwa:" << p->right->value << " " << p->left->value;
                 RBTNode * tmp = p;
                 tmp = this->nextValue(tmp);
-                if (tmp != this->guard){
-                    tmp->up->left = tmp->right; //rodzic tmp
-                    tmp->right->up = tmp->up; //dziecko tmp
 
-                    tmp->left = p->left;
-                    tmp->right = p->right;
+                tmp->up->left = tmp->right; //rodzic tmp
+                if (tmp->right != this->guard)
+                    tmp->right = tmp->up; //dziecko tmp
 
-                    toChange = tmp;
-                }
+                tmp->left = p->left;
+                tmp->right = p->right;
 
-
+                toChange = tmp;
             }
 
         }
-        if (p->up->left == p)
+        if (p->up->left == p) //p -y
             p->up->left = toChange;
-        else if (p->up->right == p) p->up->right = toChange;
+        else p->up->right = toChange;
         if (toChange != this->guard){
-            toChange->up = p->up;
+            toChange->up = p->up; //Z->up = Y->up
         }
-        this->fixRBT_D(p,toChange);
-        delete p;
+
+        //cout << "Usunieto " << p->value << endl;
+
+        if (p == this->root){
+            toChange->up = this->root->up;
+            toChange->right = this->root->right;
+            toChange->left = this->root->left;
+            this->root = toChange;
+        }
+
+        //this->fixRBT_D(p);
+
+
+        if (p != this->guard)
+            delete p;
         //return true;
-        }
-    cout << endl;
-    //cout << "Nie usuwam" << value << endl;
+        cout << "Korzen po usunieciuu: " << this->root->value << endl;
+        cout << "Dzieci: " << this->root->right->value << " " << this->root->left->value << endl;
+    }
+
     //return false;
 }
 
-RBTNode * RedBlackTree::nextValue(RBTNode * tmp){
-    RBTNode * p = tmp;
-    if (p->right != this->guard && p->right != NULL) {
-        p = p->right;
-        while (p->left != this->guard && p->left != NULL){
-            p = p->left;
-        }
-        return p;
+RBTNode * RedBlackTree::nextValue(RBTNode * p){
+    p = p->right;
+    while (p->left != this->guard){
+        p = p->left;
     }
-    return NULL;
+    return p;
 }
 
 RBTNode* RedBlackTree::findEl(int value){
     RBTNode * p = this->root;
-    while (p != this->guard && p != NULL){
+    while (p != this->guard){
         if (value == p->value){
             return p;
         }
@@ -177,81 +179,6 @@ RBTNode* RedBlackTree::findEl(int value){
 int RedBlackTree::findElement(int value) {
     if (findEl(value)== this->guard) return -1;
     else return 1;
-}
-
-void RedBlackTree::menu() {
-    cout << "MENU - drzewo czerwono-czarne\n"
-            "1. Zbuduj z pliku.\n"
-            "2. Usun.\n"
-            "3. Dodaj.\n"
-            "4. Znajdz.\n"
-            "5. Wyswietl.\n"
-            "6. Wyjdz do glownego menu.\n"
-            "Prosze wpisac odpowiednia liczbe.";
-    int chosen;
-    cin >> chosen;
-    switch(chosen){
-        case 1:
-            this->loadFile("numbers.txt");
-            break;
-        case 2:
-            cout << "Prosze podac wartosc liczby do usuniecia.\n";
-            int position;
-            cin >> position;
-            this->deleteElement(position);
-            break;
-        case 3:
-            cout << "Prosze podac liczbe do wpisania.\n";
-            cin >> position;
-            this->addElement(position,position);
-            break;
-        case 4:
-            cout << "Prosze podac liczbe do znalezienia.\n";
-            cin >> position;
-            if (this->findElement(position)!=-1)
-                cout << "Znaleziono.\n";
-            else cout << "Nie znaleziono.\n";
-            break;
-        case 5:
-            cout << *this << endl;
-            break;
-        case 6:
-            return;
-        default:
-            cout << "Prosze podac poprawna liczbe.\n";
-            cin.clear();
-            cin.sync();
-            break;
-    }
-    this->menu();
-}
-
-ostream &operator<<(ostream &os, const RedBlackTree &a) {
-    a.printRBT("","",a.root, os);
-    return os;
-
-}
-
-void RedBlackTree::printRBT(string sp, string sn, RBTNode *v, ostream &os)const {
-    string s;
-    string cr = "  ", cl= "  ", cp = "  ";
-    cr[0] = 218; cr[1] = 196;
-    cl[0] = 192; cl[1] = 196;
-    cp[0] = 179;
-
-    if(v != this->guard)
-    {
-        s = sp;
-        if(sn == cr) s[s.length() - 2] = ' ';
-        printRBT(s + cp, cr, v->right, os);
-
-        s = s.substr(0,sp.length()-2);
-        os << s << sn << v->value << "(" << v->color << ")" << endl;
-
-        s = sp;
-        if(sn == cl) s[s.length() - 2] = ' ';
-        printRBT(s + cp, cl, v->left, os);
-    }
 }
 
 void RedBlackTree::fixRBT_A(RBTNode * pNode) {
@@ -303,122 +230,202 @@ void RedBlackTree::fixRBT_A(RBTNode * pNode) {
 
 }
 
-void RedBlackTree::fixRBT_D(RBTNode *pNode,RBTNode * toChange) {
-    RBTNode *p;
+void RedBlackTree::fixRBT_D(RBTNode *pNode) {
+    RBTNode * toChange, *p;
     if(pNode->left != this->guard) toChange = pNode->left;
     else              toChange = pNode->right;
     if(pNode->color == 'B')   // Naprawa struktury drzewa czerwono-czarnego
         while((toChange != root) && (toChange->color == 'B')) {
             if(toChange == toChange->up->left) {
-            p = toChange->up->right;
-
-            if(p->color == 'R') {
-                p->color = 'B';
-                toChange->up->color = 'R';
-                rot_L(toChange->up);
                 p = toChange->up->right;
-            }
 
-            if((p->left->color == 'B') && (p->right->color == 'B')){// Przypadek 2
-                p->color = 'R';
-                toChange = toChange->up;
-                continue;
-            }
+                if(p->color == 'R') {
+                    p->color = 'B';
+                    toChange->up->color = 'R';
+                    rot_L(toChange->up);
+                    p = toChange->up->right;
+                }
 
-            if(p->right->color == 'B'){// Przypadek 3
-                p->left->color = 'B';
-                p->color = 'R';
-                rot_R(p);
-                p = toChange->up->right;
-            }
+                if((p->left->color == 'B') && (p->right->color == 'B')){// Przypadek 2
+                    p->color = 'R';
+                    toChange = toChange->up;
+                    continue;
+                }
 
-            p->color = toChange->up->color; // Przypadek 4
-            toChange->up->color = 'B';
-            p->right->color = 'B';
-            rot_L(toChange->up);
-            toChange = root;         // To spowoduje zakończenie pętli
-        }
-        else {// Przypadki lustrzane
-            p = toChange->up->left;
+                if(p->right->color == 'B'){// Przypadek 3
+                    p->left->color = 'B';
+                    p->color = 'R';
+                    rot_R(p);
+                    p = toChange->up->right;
+                }
 
-            if(p->color == 'R') { // Przypadek 1
-                p->color = 'B';
-                toChange->up->color = 'R';
-                rot_R(toChange->up);
-                p = toChange->up->left;
-            }
-
-            if((p->left->color == 'B') && (p->right->color == 'B')) { // Przypadek 2
-                p->color = 'R';
-                toChange = toChange->up;
-                continue;
-            }
-
-            if(p->left->color == 'B'){ // Przypadek 3
+                p->color = toChange->up->color; // Przypadek 4
+                toChange->up->color = 'B';
                 p->right->color = 'B';
-                p->color = 'R';
-                rot_L(p);
-                p = toChange->up->left;
+                rot_L(toChange->up);
+                toChange = root;         // To spowoduje zakończenie pętli
             }
+            else {// Przypadki lustrzane
+                p = toChange->up->left;
 
-            p->color = toChange->up->color;  // Przypadek 4
-            toChange->up->color = 'B';
-            p->left->color = 'B';
-            rot_R(toChange->up);
-            toChange = root;         // To spowoduje zakończenie pętli
+                if(p->color == 'R') { // Przypadek 1
+                    p->color = 'B';
+                    toChange->up->color = 'R';
+                    rot_R(toChange->up);
+                    p = toChange->up->left;
+                }
+
+                if((p->left->color == 'B') && (p->right->color == 'B')) { // Przypadek 2
+                    p->color = 'R';
+                    toChange = toChange->up;
+                    continue;
+                }
+
+                if(p->left->color == 'B'){ // Przypadek 3
+                    p->right->color = 'B';
+                    p->color = 'R';
+                    rot_L(p);
+                    p = toChange->up->left;
+                }
+
+                p->color = toChange->up->color;  // Przypadek 4
+                toChange->up->color = 'B';
+                p->left->color = 'B';
+                rot_R(toChange->up);
+                toChange = root;         // To spowoduje zakończenie pętli
+            }
         }
-    }
 
     toChange->color = 'B';
 }
 
 void RedBlackTree::rot_R(RBTNode *pNode) {
-    RBTNode * rbtNode, * p;
+    RBTNode * B, * p;
 
-    rbtNode = pNode->left;
-    if(rbtNode != this->guard)
+    B = pNode->left;
+    if(B != this->guard)
     {
         p = pNode->up;
-        pNode->left = rbtNode->right;
+        pNode->left = B->right;
         if(pNode->left != this->guard) pNode->left->up = pNode;
 
-        rbtNode->right = pNode;
-        rbtNode->up = p;
-        pNode->up = rbtNode;
+        B->right = pNode;
+        B->up = p;
+        pNode->up = B;
 
         if(p != this->guard)
         {
-            if(p->left == pNode) p->left = rbtNode; else p->right = rbtNode;
+            if(p->left == pNode) p->left = B; else p->right = B;
         }
-        else root = rbtNode;
+        else root = B;
     }
-
+    if (pNode == this->root){
+        this->root = B;
+    }
 }
 
 void RedBlackTree::rot_L(RBTNode *pNode) {
-    RBTNode * rbtNode, * p;
+    RBTNode * B, * p;
 
-    rbtNode = pNode->right;
-    if(rbtNode != this->guard)
+    B = pNode->right;
+    if(B != this->guard)
     {
         p = pNode->up;
-        pNode->right = rbtNode->left;
+        pNode->right = B->left;
         if(pNode->right != this->guard) pNode->right->up = pNode;
 
-        rbtNode->left = pNode;
-        rbtNode->up = p;
-        pNode->up = rbtNode;
+        B->left = pNode;
+        B->up = p;
+        pNode->up = B;
 
         if(p != this->guard)
         {
-            if(p->left == pNode) p->left = rbtNode; else p->right = rbtNode;
+            if(p->left == pNode) p->left = B; else p->right = B;
         }
-        else root = rbtNode;
+        else root = B;
     }
+    if (pNode == this->root){
+        this->root = B;
+    }
+}
+
+void RedBlackTree::printRBT(string sp, string sn, RBTNode *v, ostream &os)const {
+    string s;
+    string cr = "  ", cl= "  ", cp = "  ";
+    cr[0] = 218; cr[1] = 196;
+    cl[0] = 192; cl[1] = 196;
+    cp[0] = 179;
+
+    if(v != this->guard)
+    {
+        s = sp;
+        if(sn == cr) s[s.length() - 2] = ' ';
+        printRBT(s + cp, cr, v->right, os);
+
+        s = s.substr(0,sp.length()-2);
+        os << s << sn << v->value << "(" << v->color << ")" << endl;
+
+        s = sp;
+        if(sn == cl) s[s.length() - 2] = ' ';
+        printRBT(s + cp, cl, v->left, os);
+    }
+}
+
+ostream &operator<<(ostream &os, const RedBlackTree &a) {
+    a.printRBT("","",a.root, os);
+    return os;
+
 }
 
 void RedBlackTree::print(std::ostream &str) const {
     str << *this;
+}
+
+void RedBlackTree::menu() {
+    cout << "MENU - drzewo czerwono-czarne\n"
+            "1. Zbuduj z pliku.\n"
+            "2. Usun.\n"
+            "3. Dodaj.\n"
+            "4. Znajdz.\n"
+            "5. Wyswietl.\n"
+            "6. Wyjdz do glownego menu.\n"
+            "Prosze wpisac odpowiednia liczbe.";
+    int chosen;
+    cin >> chosen;
+    switch(chosen){
+        case 1:
+            this->loadFile("numbers.txt");
+            break;
+        case 2:
+            cout << "Prosze podac wartosc liczby do usuniecia.\n";
+            int position;
+            cin >> position;
+            this->deleteElement(position);
+            break;
+        case 3:
+            cout << "Prosze podac liczbe do wpisania.\n";
+            cin >> position;
+            this->addElement(position,position);
+            break;
+        case 4:
+            cout << "Prosze podac liczbe do znalezienia.\n";
+            cin >> position;
+            if (this->findElement(position)!=-1)
+                cout << "Znaleziono.\n";
+            else cout << "Nie znaleziono.\n";
+            break;
+        case 5:
+            cout << *this << endl;
+            break;
+        case 6:
+            return;
+        default:
+            cout << "Prosze podac poprawna liczbe.\n";
+            cin.clear();
+            cin.sync();
+            break;
+    }
+    this->menu();
 }
 
 int RedBlackTree::getElement(int position) {
